d2943150be6b0d97e70182ee841c72d4a3299d69
Ahmad Ibrahim <ahm.ibr@hotmail.com>
2022-06-09 14:13:12 -0700

    Simplify whosn-core server

--- server/src/whosn-core/main.go
+++ server/src/whosn-core/main.go
@@ -1,22 +1,33 @@
 package main
 
 import (
-	"github.com/gin-gonic/contrib/static"
-	"github.com/gin-gonic/gin"
+	"fmt"
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
 )
 
+func homePage(w http.ResponseWriter, r *http.Request) {
+	ll := log.WithFields(log.Fields{"endpoint": "homePage"})
+	ll.Println("Endpoint Hit")
+
+	_, err := fmt.Fprintf(w, "Welcome to the HomePage!")
+	if err != nil {
+		ll.Warn("Failed to write a response")
+		return
+	}
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+
+	address := ""
+	port := "8080"
+	log.Infof("Starting http server on port %v", port)
+
+	log.Fatal(http.ListenAndServe(address+":"+port, nil))
+}
+
 func main() {
-
-	r := gin.Default()
-
-	// Dont worry about this line just yet, it will make sense in the Dockerise bit!
-	r.Use(static.Serve("/", static.LocalFile("./web", true)))
-	api := r.Group("/api")
-	api.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-
-	r.Run()
+	handleRequests()
 }
