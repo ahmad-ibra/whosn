e6564bfd99e055b4d989205e1ad300431f74b9fa
Ahmad Ibrahim <ahm.ibr@hotmail.com>
2022-06-09 23:55:24 -0700

    Add api/v1/returnAllGroups test endpoint

--- server/src/whosn-core/cmd/main.go
+++ server/src/whosn-core/cmd/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
+	"time"
+
+	"github.com/Ahmad-Ibra/whosn-core/internal/data"
 
 	log "github.com/sirupsen/logrus"
 )
+
+var Groups []data.Group
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	ll := log.WithFields(log.Fields{"endpoint": "homePage"})
@@ -19,8 +25,15 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func returnAllGroups(w http.ResponseWriter, r *http.Request) {
+	ll := log.WithFields(log.Fields{"endpoint": "returnAllGroups"})
+	ll.Println("Endpoint Hit")
+	json.NewEncoder(w).Encode(Groups)
+}
+
 func handleRequests() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/api/v1/groups", returnAllGroups)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -32,5 +45,29 @@ func handleRequests() {
 }
 
 func main() {
+	Groups = []data.Group{
+		{
+			ID:        1,
+			Name:      "bobcorn",
+			OwnerID:   1,
+			CreatedAt: time.Time{},
+			UpdatedAt: time.Time{},
+		},
+		{
+			ID:        2,
+			Name:      "balbooliches",
+			OwnerID:   1,
+			CreatedAt: time.Time{},
+			UpdatedAt: time.Time{},
+		},
+		{
+			ID:        3,
+			Name:      "jiggers",
+			OwnerID:   2,
+			CreatedAt: time.Time{},
+			UpdatedAt: time.Time{},
+		},
+	}
+
 	handleRequests()
 }
