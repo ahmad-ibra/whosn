3beddb4d28c80c3e32535f550962cf039304b40c
Ahmad Ibrahim <ahm.ibr@hotmail.com>
2022-06-10 09:58:22 -0700

    Add link to the event

--- server/src/whosn-core/internal/data/models.go
+++ server/src/whosn-core/internal/data/models.go
@@ -8,13 +8,14 @@ import (
 type Event struct {
 	ID         uint64    `json:"id"`
 	Name       string    `json:"name"`
+	OwnerID    uint64    `json:"owner_id"`
 	StartTime  time.Time `json:"start_time"`
 	Location   string    `json:"location"`
 	MinUsers   uint64    `json:"min_users"`
 	MaxUsers   uint64    `json:"max_users"`
 	Price      float64   `json:"price"`
 	IsFlatRate bool      `json:"is_flat_rate"`
-	OwnerID    uint64    `json:"owner_id"`
+	Link       string    `json:"link"`
 	CreatedAt  time.Time `json:"created_at"`
 	UpdatedAt  time.Time `json:"updated_at"`
 }
