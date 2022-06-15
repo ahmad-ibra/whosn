32e6d152a6c9e162d97a31624b7a513f81910007
Ahmad Ibrahim <ahm.ibr@hotmail.com>
2022-06-15 15:30:58 -0700

    Test remove abort with status 204

--- server/src/whosn-core/internal/middleware/cors.go
+++ server/src/whosn-core/internal/middleware/cors.go
@@ -11,10 +11,10 @@ func CORS() gin.HandlerFunc {
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Header("Access-Control-Allow-Methods", "POST, HEAD, PATCH, OPTIONS, GET, PUT")
 
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
+		//if c.Request.Method == "OPTIONS" {
+		//	c.AbortWithStatus(204)
+		//	return
+		//}
 
 		c.Next()
 	}
