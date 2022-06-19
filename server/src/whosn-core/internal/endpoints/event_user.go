72043a1f7276258af1c2961b817a3d096add55ba
Ahmad Ibrahim <ahm.ibr@hotmail.com>
2022-06-18 19:11:34 -0700

    Ensure we can only join events that exist

--- server/src/whosn-core/internal/endpoints/event_user.go
+++ server/src/whosn-core/internal/endpoints/event_user.go
@@ -27,10 +27,20 @@ func JoinEvent(ctx *gin.Context) {
 	ll := log.WithFields(log.Fields{"endpoint": "JoinEvent", "actorID": actorID, "eventID": eventID})
 	ll.Println("Endpoint Hit")
 
+	// check that event exists
+	_, err := ds.GetEventByID(eventID)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		ctx.Abort()
+		return
+	}
+
+	// check that actor hasn't already joined the event
 	eventUser, err := ds.GetEventUserByEventIDUserID(eventID, actorID)
 	if err != nil {
 		// TODO: once custom error type with status is created finish off this logic, for now assuming its NOTFOUND
 		// if error is NOTFOUND {
+		// construct and join the event
 		eventUser.Construct(eventID, actorID)
 		err = ds.InsertEventUser(*eventUser)
 		if err != nil {
