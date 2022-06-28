ccc04bd8482567c2f82e85166ba04367719d5941
Ahmad Ibrahim <ahm.ibr@hotmail.com>
2022-06-28 12:43:40 -0700

    More setup for the detailed events page

--- client/whosn-webapp/src/routes/DetailedEvent.js
+++ client/whosn-webapp/src/routes/DetailedEvent.js
@@ -35,26 +35,57 @@ const DetailedEvent = () => {
         return await res.json()
     }
 
+    // Fetch Joined Event
+    // const fetchJoined = async (id) => {
+    //     const res = await fetch(
+    //         // TODO: need to implement this endpoint on the backend. ideally it returns both the joined and waitlist items
+    //         `http://${backendAddress}/api/v1/secured/event_users/${id}`,
+    //         {
+    //             headers: {
+    //                 'Content-type': 'application/json',
+    //                 Authorization: auth(),
+    //             },
+    //         }
+    //     )
+
+    //     return await res.json()
+    // }
+
     const found = !(event.error != null)
 
     return (
         <div>
             {!found && <NotFound />}
             {found && (
-                <div className="container">
-                    <h2>{event.name}</h2>
-                    <p>time: {event.time}</p>
-                    <p>location: {event.location}</p>
-                    <p>min participants: {event.min_users}</p>
-                    <p>max participants: {event.max_users}</p>
-                    <p>price: {event.price}</p>
-                    <p>link: {event.link}</p>
+                <div>
+                    <div className="container">
+                        {/* TODO: add join/leave event button (depends on if they've joined event or not) */}
+                        {/* TODO: add delete event button (depends on if they're the owner of the event or not) */}
+                        {/* TODO: add share event button (depends on if they're the owner of the event or not) */}
+                        <h2>Event Details</h2>
+                        <p>name: {event.name}</p>
+                        <p>time: {event.time}</p>
+                        <p>location: {event.location}</p>
+                        <p>min participants: {event.min_users}</p>
+                        <p>max participants: {event.max_users}</p>
+                        <p>price: {event.price}</p>
+                        {/* TODO: update this to event.max_users - people that are joined */}
+                        <p>spots left: {event.max_users}</p>
+                        <p>link: {event.link}</p>
+                    </div>
+                    <div className="container">
+                        <h2>In</h2>
+                        <p>TODO: get people that joined</p>
+                        {/* TODO: Create UserList component which lists all users that are in from fetchJoined */}
+                    </div>
+                    <div className="container">
+                        <h2>Wait List</h2>
+                        <p>TODO: get people that are on the waitlist</p>
+                        {/* TODO: Create UserList component which lists all users that are on the waitlist from fetchJoined */}
+                    </div>
                 </div>
             )}
         </div>
-        // TODO: add join/leave event button (depends on if they've joined event or not)
-        // TODO: add delete event button (depends on if they're the owner of the event or not)
-        // TODO: add share event button (depends on if they're the owner of the event or not)
     )
 }
 
