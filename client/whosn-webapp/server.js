6985dbab27c1a55fd16a4d1bb2402be1dc458c97
Ahmad Ibrahim <ahm.ibr@hotmail.com>
2022-06-28 08:14:35 -0700

    try again

--- client/whosn-webapp/server.js
+++ client/whosn-webapp/server.js
@@ -8,14 +8,8 @@ const port = process.env.PORT || 3000
 app.use(express.static(path.join(__dirname, 'build')))
 
 // This route serves the React app
-if (process.env.ENVIRONMENT === 'production') {
-    app.get('/*', (req, res) =>
-        res.sendFile(path.join(__dirname, 'build', 'index.html'))
-    )
-} else {
-    app.get('/', (req, res) =>
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-    )
-}
+app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+)
 
 app.listen(port, () => console.log(`Server listening on port ${port}`))
