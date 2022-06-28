66b5b10a964b1fa3a0c4e10bb05aa1ad65676107
Ahmad Ibrahim <ahm.ibr@hotmail.com>
2022-06-28 10:39:22 -0700

    Simplify privateroute

--- client/whosn-webapp/src/auth/PrivateRoute.js
+++ client/whosn-webapp/src/auth/PrivateRoute.js
@@ -12,14 +12,8 @@ const PrivateRoute = ({ children }) => {
     // seconds elapsed since January 1, 1970 UTC
     const now = Math.round(new Date().getTime() / 1000)
 
-    // check not expired (we consider it expired 5 minutes early)
-    if (decodedToken.exp - 300 < now) {
-        localStorage.removeItem('jwt')
-        return <Navigate to="/login" />
-    }
-
-    // check issuer
-    if (decodedToken.iss !== 'whosn-core') {
+    // check not expired (we consider it expired 5 minutes early) and check issuer
+    if (decodedToken.exp - 300 < now || decodedToken.iss !== 'whosn-core') {
         localStorage.removeItem('jwt')
         return <Navigate to="/login" />
     }
