574a582537c5c8c80472db1f8afb7c75514cfa5a
Ahmad Ibrahim <ahm.ibr@hotmail.com>
2022-07-04 10:34:58 -0700

    Implement logout button in navbar

--- client/whosn-webapp/src/components/Navbar.js
+++ client/whosn-webapp/src/components/Navbar.js
@@ -1,11 +1,15 @@
 import '../styles/Navbar.css'
 import { Link } from 'react-router-dom'
 
+const unsetToken = () => {
+    localStorage.removeItem('jwt')
+}
+
 const Navbar = () => {
     return (
         <section className="navbar">
-            <Link to="/login" className="navbar-item">
-                Login
+            <Link to="/login" className="navbar-item" onClick={unsetToken}>
+                Logout
             </Link>
             <Link to="/" className="navbar-item">
                 Home
