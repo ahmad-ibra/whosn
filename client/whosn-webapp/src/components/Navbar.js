6960fefa56f9f6c117be6d8bfdbfa1d30caab98c
Ahmad Ibrahim <ahm.ibr@hotmail.com>
2022-07-19 19:00:31 -0700

    Remove Profile button from navbar

--- client/whosn-webapp/src/components/Navbar.js
+++ client/whosn-webapp/src/components/Navbar.js
@@ -14,9 +14,6 @@ const Navbar = () => {
             <Link to="/" className="navbar-item">
                 Home
             </Link>
-            <Link to="/profile" className="navbar-item">
-                Profile
-            </Link>
         </section>
     )
 }
